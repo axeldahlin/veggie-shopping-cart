66995f2f123b13bf8de93e7533f4e0202a79decb
Axel <axdahlin@gmail.com>
2018-09-23 14:35:22 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+import Layout from './hoc/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+     <Layout />
+    );
+  }
+}
+
+export default App;
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Aux from '../Aux/Aux';
+
+class Layout extends Component {
+  render () {
+    return (
+      <Aux>
+        <p>This is the layout</p>
+        <h1>Hej hopp</h1>
+      </Aux>
+    );
+
+  }
+
+}
+export default Layout;
